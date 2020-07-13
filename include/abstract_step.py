02842c3c157e837ccd3f8ffb0608756fd34fac15
Dominik <dominik.otto@izi.fraunhofer.de>
2020-07-13 11:31:55 +0200

    undeterminable states in state string

--- include/abstract_step.py
+++ include/abstract_step.py
@@ -879,9 +879,17 @@ class AbstractStep(object):
                         disable=not progress, leave=False)
         try:
             for run in run_iter:
-                if isinstance(run, str):
-                    run = self.get_run(run)
-                state = run.get_state(do_hash=do_hash)
+                retries = 5
+                state = self.get_pipeline().states.UNDETERMINABLE
+                while retries > 0:
+                    try:
+                        if isinstance(run, str):
+                            run = self.get_run(run)
+                        state = run.get_state(do_hash=do_hash)
+                    except Exception:
+                        retries -= 1
+                        continue
+                    break
                 if state not in count:
                     count[state] = 0
                 count[state] += 1
