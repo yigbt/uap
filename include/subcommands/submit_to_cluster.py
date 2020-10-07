4c018753239b02a15fa02368da53e9550b82102d
canzler <sebastian.canzler@ufz.de>
2020-10-07 14:21:30 +0200

    add quotes correctly to qsub output path

--- include/subcommands/submit_to_cluster.py
+++ include/subcommands/submit_to_cluster.py
@@ -219,7 +219,7 @@ def main(args):
         submit_script_args += p.get_cluster_command_cli_option(
             'set_job_name', step_name)
 
-        out_file = "," + os.path.join(step.get_output_directory(),
+        out_file = "'" + os.path.join(step.get_output_directory(),
                                 '.' + long_task_id_with_date + '.uapout') + "'"
         submit_script_args.append(p.get_cluster_command('set_stdout'))
         submit_script_args.append(out_file)
