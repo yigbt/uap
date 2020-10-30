3bd90a2b84bd735d3548e6ad1f1d921d96fed2a6
canzler <sebastian.canzler@ufz.de>
2020-10-30 08:54:43 +0100

    Use python3 in bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+
 # This script creates a virtual Python environment in ./python-env with all required libraries set up.
 # To use it, call Python at ./python-env/bin/python.
 
 # Test if everything is available
+python3 --version >/dev/null 2>&1 || missing=$missing"[bootstrap.sh] Python3 is required but it's not installed.\n"
 virtualenv --version >/dev/null 2>&1 || missing=$missing"[bootstrap.sh] Python virtualenv is required but it's not installed.\n"
 gcc --version >/dev/null 2>&1 || missing=$missing"[bootstrap.sh] GCC is required but it's not installed.\n"
 git --version >/dev/null 2>&1 || missing=$missing"[bootstrap.sh] GIT is required but it's not installed.\n"
+
 
 # Report errors if any
 if [[ -n "$missing" ]]; then
@@ -15,5 +18,12 @@ if [[ -n "$missing" ]]; then
     exit 1
 fi
 
-virtualenv python_env
-./python_env/bin/pip install pyyaml numpy biopython psutil cpython scipy deepdiff tqdm
+
+# get the python3 interpreter, virtualenv uses python2 by default
+pypath=`which python3`
+
+# get the uap directory
+dir=`dirname $0`
+
+virtualenv --python=$pypath $dir/python_env
+$dir/python_env/bin/pip install pyyaml numpy biopython psutil cpython scipy deepdiff tqdm
